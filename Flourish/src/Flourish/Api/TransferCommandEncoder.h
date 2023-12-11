a58b34c0c4fa18b80ccc44ed9119c366563830c3
TheApplePieGod <evant.002@gmail.com>
2023-12-10 20:31:48 -0500

    Add doc comment

--- Flourish/src/Flourish/Api/TransferCommandEncoder.h
+++ Flourish/src/Flourish/Api/TransferCommandEncoder.h
@@ -14,6 +14,7 @@ namespace Flourish
         virtual void FlushBuffer(Buffer* buffer) = 0;
         virtual void CopyTextureToBuffer(Texture* texture, Buffer* buffer, u32 layerIndex = 0, u32 mipLevel = 0) = 0;
         virtual void CopyBufferToTexture(Texture* texture, Buffer* buffer, u32 layerIndex = 0, u32 mipLevel = 0) = 0;
+        // Bytes
         virtual void CopyBufferToBuffer(Buffer* src, Buffer* dst, u32 srcOffset, u32 dstOffset, u32 size) = 0;
     };
 }
